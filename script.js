c3f300c06e6948862eda223a52f036ee41611e4b
tsogto1 <tsogtbayarkhosbayar@gmail.com>
2026-09-14 19:52:31 +0800

    k6 lab2 project setup

---
+++ script.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  vus: 5,
+  duration: '30s',
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+
+  check(res, {
+    'status 200 байна': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
